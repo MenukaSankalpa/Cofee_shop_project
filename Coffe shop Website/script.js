b53e1efeaee5cf6cca39b6bc851fbc1a12c5fb28
MenukaSankalpa <menuka.sankalpa@gmail.com>
2024-12-27 15:31:54 +0530

    create some javascript part

--- Coffe shop Website/script.js
+++ Coffe shop Website/script.js
@@ -8,8 +8,25 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick =()=>{
     search.classList.toggle('active');
-};
+    navbar.classList.remove('active');
+}
 
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick =()=>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+/*document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+    const menuIcon = document.querySelector('#menu-icon');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+        search.classList.remove('active');
+    });
+});*/
 
 
 let header = document.querySelector('header');
